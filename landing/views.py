47ab3fe134af43078b6f3502d3cb742663293784
satish <satish.kumar@careers360.com>
2020-06-10 23:52:09 +0530

    test1 pushed for testing

--- landing/views.py
+++ landing/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>This is home page</h1>')
+	return HttpResponse('<h1>This is home page1</h1>')

62ed770030afd272cc6463940f1e312a86400bbc
satish <satish.kumar@careers360.com>
2020-08-03 19:58:27 +0530

    old urls file removed

--- shopkeeper/backend/landing/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.conf.urls import url, include
-# from landing.views import user_homepage
-# app_name = 'landing'
-# urlpatterns = [
-#     url(r'^$', user_homepage, name = 'landing_homepage'),
-    
-# ]
--- shopkeeper/backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.conf.urls import url, include
-# from users.views import register, login_user, logout_user
-# app_name = 'users'
-# urlpatterns = [
-#     url(r'^$', register, name = 'register'),
-#     url(r'^login', login_user, name = 'login_user'),
-#     url(r'^logout', logout_user, name = 'logout_user'),
-    
-# ]

324f4d61e77da2936535e60124bb3dba9488157d
satish <satish.kumar@careers360.com>
2020-10-24 02:06:20 +0530

    change in gunicorn

--- deploy/gunicorn/gunicorn_start.bash
+++ deploy/gunicorn/gunicorn_start.bash
@@ -13,7 +13,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source /home/ubuntu/shopenv/bin/activate
+source /home/ubuntu/main/env/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
